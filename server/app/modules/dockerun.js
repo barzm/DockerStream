19c210e5c9ea11c18e2494285d38b950267e65d7
Michael Barzizza <mbarzizza1@gmail.com>
2015-06-26 18:31:13 -0400

    Start writing git repo download

--- server/app/modules/dockerun.js
+++ server/app/modules/dockerun.js
@@ -1,7 +1,55 @@
 // this is where we instantiate containers and process user pipelines
-var Docker = require('dockerode-promise'); 
+var Docker = require('dockerode-promise');
+var exec = require('exec'); 
+var ghdownload = require('github-download');
 var fs = require('fs'); //used to access dockerfile
-module.exports = function(){
-	
+var log = require("npmlog");
+var axios = require('axios');
+var Promise = require('bluebird');
+var tartar = require('tar.gz');
+
+Promise.promisifyAll(fs);
+
+module.exports = function() {
+	axios.get('https://api.github.com/repos/mbarzizza/DockerTest/tarball')
+	.then(function(response){
+		console.log('response',response)
+		return response
+	})
+	.catch(function(err){
+		if(err.status===302){
+			return axios.get(err.headers.location)
+		}
+	})
+	.then(function (response){
+		var file = fs.createWriteStream(__dirname + '/temp/repo.tar.gz')
+		console.log('response',response)
+		file.write(response.data)
+		// response.on('data',function(chunk){
+		// 	console.log("stream")
+		// 	file.write(chunk)
+		// })
+		// .on('end',function(){
+		// 	file.end()
+		// 	return file
+
+		// })
+		// return tarRepo(response.data)
+	})
+	// .then(function (file){
+	// 	console.log("FILE",file)
+	// })
+	.catch(function(err){
+		console.log("2nd err",err)
+	})
+}
+
+
+function tarRepo(buffer){
+	// console.log("BUFFER",buffer)
+	var file = fs.createWriteStream(__dirname + '/temp/repo.tar.gz')
+	return file.write(buffer)
 
 }
+
+// https://github.com/mbarzizza/DockerTest.git
--- server/app/routes/run/index.js
+++ server/app/routes/run/index.js
@@ -2,18 +2,20 @@
 var Docker = require('dockerode-promise'); 
 var router = require('express').Router();
 var exec = require('child_process').exec;
+var run = require('../../modules/dockerun.js'); 
 module.exports = router;
 
-
 router.get('/',function(req,res,next){
-	console.log('running yo shit');
+	console.log('route hit');
+
 	// var pipelineId = req.query.id; 
 	// var callbackUrl = req.query.callbackUrl; 
-	exec('cd public/applet; docker build .',function(err,stdout,stderr){
-		console.log('err', err);
-		console.log('stdout ',stdout);
-		console.log('stderr',stderr);
-	})
+	run();
+	// exec('cd public/applet; docker build .',function(err,stdout,stderr){
+		// console.log('err', err);
+		// console.log('stdout ',stdout);
+		// console.log('stderr',stderr);
+	// })
 	res.send("okay"); 
 
 })
