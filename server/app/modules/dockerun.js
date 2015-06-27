fb508d6861d1a39e33fcd4bf52f036d716776f1e
Michael Barzizza <mbarzizza1@gmail.com>
2015-06-26 20:26:27 -0400

    still trying

--- server/app/modules/dockerun.js
+++ server/app/modules/dockerun.js
@@ -8,40 +8,54 @@ var axios = require('axios');
 var Promise = require('bluebird');
 var tartar = require('tar.gz');
 
+
 Promise.promisifyAll(fs);
 
 module.exports = function() {
-	axios.get('https://api.github.com/repos/mbarzizza/DockerTest/tarball')
-	.then(function(response){
-		console.log('response',response)
-		return response
-	})
-	.catch(function(err){
-		if(err.status===302){
-			return axios.get(err.headers.location)
-		}
-	})
-	.then(function (response){
-		var file = fs.createWriteStream(__dirname + '/temp/repo.tar.gz')
-		console.log('response',response)
-		file.write(response.data)
-		// response.on('data',function(chunk){
-		// 	console.log("stream")
-		// 	file.write(chunk)
-		// })
-		// .on('end',function(){
-		// 	file.end()
-		// 	return file
+	console.log("executing run module")
+	var docker = new Docker({
+		socketPath: '/var/run/docker.sock',
+		host: '192.168.1.123'
+		// port: process.env.DOCKER_PORT || 2375
+	}); 
+	console.log(docker);
+	docker.buildImage(__dirname + '/temp/test.tar',{t:'node'},function(err,response){
+		console.log('error ', err);
+		console.log('================');
+		console.log('response ',response);
 
-		// })
-		// return tarRepo(response.data)
 	})
-	// .then(function (file){
-	// 	console.log("FILE",file)
+	// axios.get('https://api.github.com/repos/mbarzizza/DockerTest/tarball')
+	// .then(function(response){
+	// 	console.log('response',response)
+	// 	return response
 	// })
-	.catch(function(err){
-		console.log("2nd err",err)
-	})
+	// .catch(function(err){
+	// 	if(err.status===302){
+	// 		return axios.get(err.headers.location)
+	// 	}
+	// })
+	// .then(function (response){
+	// 	var file = fs.createWriteStream(__dirname + '/temp/repo.tar.gz')
+	// 	console.log('response',response)
+	// 	file.write(response.data)
+	// 	// response.on('data',function(chunk){
+	// 	// 	console.log("stream")
+	// 	// 	file.write(chunk)
+	// 	// })
+	// 	// .on('end',function(){
+	// 	// 	file.end()
+	// 	// 	return file
+
+	// 	// })
+	// 	// return tarRepo(response.data)
+	// })
+	// // .then(function (file){
+	// // 	console.log("FILE",file)
+	// // })
+	// .catch(function(err){
+	// 	console.log("2nd err",err)
+	// })
 }
 
 
