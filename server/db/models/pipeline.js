64f281aae02138f4a5fd2808d4cce2bb7a6710c8
Michael Barzizza <mbarzizza1@gmail.com>
2015-06-27 18:34:37 -0400

    fixing small bug in pipeline model

--- server/db/models/pipeline.js
+++ server/db/models/pipeline.js
@@ -2,7 +2,7 @@
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-	user: { type: Schema.Types.ObjectId, ref: 'User' },
+	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 	pipeline: [{
 		gitUrl: { type: String, required: true },
 		order: { type: Number, required: true }
