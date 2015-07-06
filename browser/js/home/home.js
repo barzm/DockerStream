7373bf4d374d3ffe2f95fcc922aecaf6ac778f71
Robbie Ferguson <robbieferguson139@gmail.com>
2015-07-06 16:22:00 -0400

    a lot of changes

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,20 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeCtrl', 
+        resolve: {
+        	user: function (AuthService) {
+        		return AuthService.getLoggedInUser();
+        	}
+        }
     });
 });
+
+app.controller('HomeCtrl', function ($scope, AuthService, user) {
+	$scope.user = user;
+	$scope.gitLogin = function () {
+		AuthService.gitLogin();
+	}
+
+})
