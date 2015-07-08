2527665199e7fbfb88b45adf15256cca5bb3162f
Rachel Charow <rachel.charow@gmail.com>
2015-07-07 20:48:57 -0400

    working on help page

--- browser/js/help/help.js
+++ browser/js/help/help.js
@@ -9,5 +9,11 @@ app.config(function($stateProvider) {
 });
 
 app.controller('HelpCtrl',function($scope){
-
+  $scope.tabs = [
+    { title:'About Ahab', content:'Dynamic content 1' },
+    { title:'Preparing Pipes', content:'Dynamic content 1' },
+    { title:'Adding Pipes', content:'Dynamic content 2'},
+    { title:'Editing Pipelines', content:'Dynamic content 2'},
+    { title:'Running a Pipeline', content:'Dynamic content 2'}
+  ];
 })
