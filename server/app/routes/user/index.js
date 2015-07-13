fae34e57c4ae43004fb281fb8f3d81a833d90b16
Robbie Ferguson <robbieferguson139@gmail.com>
2015-07-13 15:30:58 -0400

    working user put route

--- server/app/routes/user/index.js
+++ server/app/routes/user/index.js
@@ -5,16 +5,16 @@ var request = require('request-promise');
 var User = require('mongoose').model('User');
 var Pipeline = require('mongoose').model('Pipeline');
 var cleanup = require('../../modules/dockercleanup');
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
 	User.findById(req.user._id)
-	.populate('pipelines')
-	.exec()
-	.then(function (user) {
-		res.json(user);
-	})
-	.then(null, next)
+		.populate('pipelines')
+		.exec()
+		.then(function(user) {
+			res.json(user);
+		})
+		.then(null, next)
 })
-router.put('/', function (req, res, next) {
+router.put('/', function(req, res, next) {
 	console.log('req.body')
 	console.log(req.body)
 	var updatedPipeline = req.body.pipelines;
@@ -23,31 +23,37 @@ router.put('/', function (req, res, next) {
 	var counter = 0;
 	var len = Object.keys(updatedPipeline).length
 	var newPipeline;
-	//NOT WORKING - no image id
-	// cleanup.deleteImage(req.body.image.imageId)
-	// .then(function(){
+
+	if (req.body.image.imageId) {
+		cleanup.deleteImage(req.body.image.imageId)
+			.then(putNewPipeline)
+			.catch(function(err) {
+				err.message = "There was a problem deleting a pipe from your pipeline: " + req.body.image;
+				err.status = 911;
+				next(err);
+			})
+	} else {
+		putNewPipeline();
+	}
+
+	function putNewPipeline() {
 		for (var pipelineObj in updatedPipeline) {
 			newPipeline = updatedPipeline[pipelineObj]['pipeline'];
 			newPipelines.push(newPipeline);
 			Pipeline.findById(updatedPipeline[pipelineObj].pipelineId)
-			.exec()
-			.then(function (pipeline) {
-				pipeline.pipeline = newPipelines.shift();
-				pipeline.save(function (err, savedPipeline) {
-					counter++;
-					pipelines.push(savedPipeline)
-					if (counter === len) res.json(pipelines);
+				.exec()
+				.then(function(pipeline) {
+					pipeline.pipeline = newPipelines.shift();
+					pipeline.save(function(err, savedPipeline) {
+						counter++;
+						pipelines.push(savedPipeline)
+						if (counter === len) res.json(pipelines);
+					})
 				})
-			})
-			.then(null,function(err){
-				err.message = "There was a problem deleting a pipe from your pipeline";
-				next(err);
-			})
+				.then(null, function(err) {
+					err.message = "There was a problem deleting a pipe from your pipeline";
+					next(err);
+				})
 		}
-	// })
-	// .catch(function(err){
-	// 	err.message ="There was a problem deleting a pipe from your pipeline: " + req.body.image;
-	// 	err.status = 911;
-	// 	next(err);
-	// })
+	}
 })
